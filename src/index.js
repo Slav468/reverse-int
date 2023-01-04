36559514b9c90ca835089cdb650b86dc032ca3e2
Slav468 <slav1988@tut.by>
2023-01-04 11:17:01 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let str = `${n}`;
+    let reverseStr;
+    console.log(str[0]);
+
+    if (str[0] === "-") {
+        reverseStr =
+            str[str.length - 1] + str.slice(2, str.length - 1) + str[1];
+    } else {
+        reverseStr =
+            str[str.length - 1] + str.slice(1, str.length - 1) + str[0];
+    }
+
+    return +reverseStr;
+};
